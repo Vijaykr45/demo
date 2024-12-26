c283a3cc83c7edbd5610550c7dc14babf4907123
Vijay <20202054.ece@gmail.com>
2024-12-26 11:39:52 +0530

    login commited

---
+++ app.js
@@ -0,0 +1,19 @@
+function great(name) {
+    console.log(`Hello goodmorning,${name}`);
+
+}
+
+great(`wasteFellows`) 
+
+function greet2(name) {
+    return (`hello goodmorning, ${name}`);
+}
+let res = greet2 (`j spider`);
+console.log(res);
+let sum = function (a, b) {
+    return a +  b ;
+}
+console.log(sum(5,10));
+
+
+
